56dca85da7bc70bdba8af71d669cd4bed4585e72
a-leexander <monkey.aleexander@gmail.com>
2026-01-07 10:01:44 -0300

    Agregando formulario, css y dependencias para el proyecto

--- app.js
+++ app.js
@@ -1,1 +1,48 @@
-console.log("Hola");
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
+
+const upload = multer({
+    dest: "uploads/",
+    fileFilter: (req, file, cb) => {
+        if (file.mimetype === "application/pdf") {
+            cb(null, true);
+        } else {
+            cb(new Error("Solo se permiten archivos PDF"));
+        }
+    }
+});
+
+const app = express();
+
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+app.use("/resources", express.static("public"));
+app.use("/resources", express.static(__dirname + "/public"));
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res)=>{
+    res.render("formulario");
+});
+
+//Creacion del edpoint para recibir solo una url (una imagen)
+app.post("/enviar-justificativo", upload.single("certificado"), (req, res) => {
+
+    // Datos del formulario
+    console.log(req.body);
+
+    // Archivo PDF
+    console.log(req.file);
+
+    if (!req.file) {
+        return res.send("No se subió ningún PDF");
+    }
+
+    res.send("Formulario y PDF recibidos correctamente");
+});
+
+app.listen(3200, function(){
+    console.log("Servidor creado http://localhost:3200");
+});
