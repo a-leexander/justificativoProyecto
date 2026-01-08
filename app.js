29923e59789af6648d5305686633bc1a29d9185a
a-leexander <monkey.aleexander@gmail.com>
2026-01-08 16:37:23 -0300

    agreo el limitante de peso, el solo archivos pdf, logeo por tipo y cierta parte de ingreso automatico de datos

--- app.js
+++ app.js
@@ -33,39 +33,9 @@ const multer = require("multer");
 const path = require("path");
 const { error } = require("console");
 
-const upload = multer({
-    dest: "uploads/",
-    fileFilter: (req, file, cb) => {
-        if (file.mimetype === "application/pdf") {
-            cb(null, true);
-        } else {
-            cb(new Error("Solo se permiten archivos PDF"));
-        }
-    }
-});
-
-//Creacion del edpoint para recibir solo una url (una imagen)
-app.post("/enviar-justificativo", upload.single("certificado"), (req, res) => {
-
-    // Datos del formulario
-    console.log(req.body);
-
-    // Archivo PDF
-    console.log(req.file);
-
-    
-    if (!req.file) {
-        return res.send("No se subió ningún PDF");
-    }
-
-    res.send("Formulario y PDF recibidos correctamente");
-});
 
 
 //Rutas 
-app.get("/", (req, res)=>{
-    res.render("index");
-});
 
 app.get("/login", (req, res)=>{
     res.render("login");
@@ -74,16 +44,45 @@ app.get("/home", (req, res) => {
     if (req.session.loggedin) {
         res.render("home", {
             login: true,
-            name: req.session.name
+            user: req.session.user
         });
     } else {
         res.redirect("/login");
     }
 });
 
-app.get("/formulario", (req, res)=>{
-    res.render("formulario");
+
+app.get("/formulario", (req, res) => {
+
+    if (
+        !req.session.loggedin || !req.session.user || req.session.user.tipo !== "A"
+    ) {
+        return res.redirect("/login");
+    }
+
+    const rutAlumno = req.session.user.rut;
+
+    conexion.query(
+        "SELECT * FROM Alumno WHERE rut = ?",
+        [rutAlumno],
+        (error, results) => {
+
+            if (error) {
+                console.error(error);
+                return res.send("Error en la base de datos");
+            }
+
+            if (results.length === 0) {
+                return res.send("Alumno no encontrado");
+            }
+
+            res.render("formulario", {
+                alumno: results[0]
+            });
+        }
+    );
 });
+
 
 //Autenticación
 app.post('/auth', async (req, res)=>{
@@ -103,16 +102,9 @@ app.post('/auth', async (req, res)=>{
                 })
             }else{
                 req.session.loggedin =true;
-                req.session.name = results[0].name
-                res.render("login", {
-                    alert: true,
-                    alertTitle: "Conexion exitosa",
-                    alertMessage: "Inisio de sesión correcto",
-                    alertIcon: "success",
-                    showConfirmButton: false,
-                    timer: 1500,
-                    ruta: "home"
-                })
+                req.session.user = {rut: results[0].rut, tipo: results[0].tipo};
+                res.redirect("/home");
+                console.log(req.body);
             }
         })
     }else{
@@ -147,8 +139,69 @@ app.get("/", (req, res)=>{
 app.get("/logout", (req, res)=>{
     req.session.destroy(()=>{
         res.redirect("/")
-    })
-})
+    });
+});
+
+
+
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "uploads/");
+    },
+    filename: (req, file, cb) => {
+        cb(null, `justificativo_${Date.now()}.pdf`);
+    }
+});
+
+//limitante de archivos a solo pdf
+const upload = multer({
+    storage: storage,
+    limits:{
+        fileSize: 2 * 1024 * 1024
+    },
+    fileFilter: (req, file, cb) => {
+        if (file.mimetype === "application/pdf") {
+            cb(null, true);
+        } else {
+            cb(new Error("Solo se permiten archivos PDF"));
+        }
+    }
+});
+
+//Creacion del edpoint para recibir solo una url (una imagen)
+app.post("/enviar-justificativo", upload.single("certificado"), (req, res) => {
+
+    // Datos del formulario
+    console.log(req.body);
+
+    // Archivo PDF
+    console.log(req.file);
+
+    
+    if (!req.file) {
+        return res.redirect("No se subió ningún PDF", "/formulario");
+    }
+
+    res.redirect("/home");
+});
+
+app.use((err, req, res, next) => {
+
+    if (err instanceof multer.MulterError) {
+        if (err.code === "LIMIT_FILE_SIZE") {
+            return res.send("El archivo supera el tamaño máximo permitido");
+        }
+    }
+
+    if (err) {
+        return res.send(err.message);
+    }
+
+    next();
+});
+
+
 
 app.listen(3000, function(){
     console.log("Servidor creado http://localhost:3000");
